c3fdbc50d743cef9e5796dc2d705b695c2a0931b
Mohammad Fakhruddin Babar <71979845+fakhruddinbabor7@users.noreply.github.com>
2024-05-24 14:45:34 -0700

    Update response_time.py

--- response_time.py
+++ response_time.py
@@ -1,29 +1,39 @@
+##This is the correct version of code
 import math
 
-def calculate_response_times_upto_time(C, T, total_time):
+def calculate_multiframe_response_times_upto_time(C, T, total_time):
     n = len(C)
     response_times = {i: [] for i in range(n)}
-    
-    for current_time in range(1, total_time + 1):
+
+    for current_time in range(0, total_time + 1):
         for i in range(n):
             if current_time % T[i] == 0:
                 # Task i arrives at this time
-                R_prev = C[i]
+                frame_index = (current_time // T[i]) % len(C[i])
+                current_C = C[i][frame_index]
+                R_prev = current_C
                 while True:
-                    R = C[i] + sum(math.ceil(R_prev / T[j]) * C[j] for j in range(i))
+                    R = current_C + sum(
+                        math.ceil(R_prev / T[j]) * C[j][(current_time // T[j]) % len(C[j])]
+                        for j in range(i)
+                    )
                     if R == R_prev:
                         break
                     R_prev = R
                 response_times[i].append((current_time, R))
-    
+
     return response_times
 
 # Example usage
-C = [2, 1, 2]  # Computation times
+C = [
+    [2],  # Computation times for task 1
+    [1, 2, 3],     # Computation times for task 2
+    [2]   # Computation times for task 3
+]
 T = [5, 10, 20]  # Periods
-total_time = 50  # Total time to calculate the response times for
+total_time = 40  # Total time to calculate the response times for
 
-response_times = calculate_response_times_upto_time(C, T, total_time)
+response_times = calculate_multiframe_response_times_upto_time(C, T, total_time)
 print(response_times)
 for i in range(len(C)):
     print(f"Task {i+1} response times:")
